e95a7c4cf4f0b1c5e44b2b64359a27c101ac180e
kerat6 <t.ahmadieh@outlook.com>
2024-10-18 10:38:32 -0400

    added reset and new grid. adjusted etch a sketch

--- javascript.js
+++ javascript.js
@@ -1,16 +1,18 @@
+
 for (let i = 0; i < (16 * 16); i++) {
     const container = document.querySelector("#container");
     const div = document.createElement("div");
     div.setAttribute("class", "block")
     div.style.display = "flex";
-    div.style.height = '6.25vh';
-    div.style.width = '6.25vw';
+    div.style.height = `${1000 / 16}px`;
+    div.style.width = `${1000 / 16}px`;
+    div.style.flexGrow = 1;
     div.style.alignItems = "center";
     div.style.backgroundColor = "white";
     container.style.border = "solid black";
     div.style.border = "solid black";
-    div.style.margin = '2px';
     container.appendChild(div)
+
 }
 
 
@@ -40,3 +42,52 @@ blocks.forEach((block) => {
         }
     });
 });
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const buttonClick = button.id;
+        if (buttonClick == 'reset') {
+            const blocks = document.querySelectorAll('.block')
+            blocks.forEach(block => {
+                block.style.backgroundColor = "white";
+
+            });
+        }
+        else if (buttonClick == 'newGrid') {
+            const gridSize = Number(prompt("How many squares would you like per side?", '16'))
+            container.innerHTML = ''
+            for (let i = 0; i < (gridSize * gridSize); i++) {
+                const container = document.querySelector("#container");
+                const div = document.createElement("div");
+                div.setAttribute("class", "block")
+                div.style.display = "flex";
+                div.style.height = `${1000 / gridSize}px`;
+                div.style.width = `${1000 / gridSize}px`;
+                div.style.alignItems = "center";
+                div.style.backgroundColor = "white";
+                div.style.border = "solid black";
+                div.style.borderWidth = '1px';
+                container.appendChild(div);
+
+            }
+            const blocks = document.querySelectorAll('.block')
+            blocks.forEach((block) => {
+                block.addEventListener('mouseenter', () => {
+                    if (block.style.backgroundColor == "white") {
+                        block.style.backgroundColor = getRandomColor();
+                    } else if (block.style.backgroundColor !== "white") {
+                        block.style.backgroundColor = "white";
+                    }
+                });
+            });
+        }
+    });
+});
+
+
+
+// reset all tiles
+// blocks.forEach((block) => {
+//     block.style.backgroundColor = "white"
+// })
